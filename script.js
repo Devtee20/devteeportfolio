9524718c160e9cf615f7a55b46699b48ed8767dc
unknown <familusibisola20@gmail.com>
2026-04-01 16:14:02 +0100

    files updated

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+
+document.querySelectorAll("#nav-links a").forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+    });
+});
+
+
+
+
 let contactForm = document.getElementById('contact-form');
 let contactMessage = document.getElementById('contactMessage')
 
